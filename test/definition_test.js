4472ffd68b43458e8097a69458e2888d1d0c0588
Andrew Paradise <andrew@cantinaconsulting.com>
2012-07-26 15:08:49 -0400

    add match method on definition to help with scanning

--- test/definition_test.js
+++ test/definition_test.js
@@ -1,3 +1,4 @@
+var jsdom = require('jsdom');
 
 var MDPlus = require('../lib/md_plus.js');
 
@@ -10,6 +11,47 @@ exports.definition = {
             test.ok(errors.errorCount() > 0, "should have at least one validation message");
             test.ok(error, "should have an error for the tag field");
             test.done();
+        },
+
+        requires_classRef: function (test) {
+            var def = new MDPlus.Definition({});
+            var errors = def.validate();
+            var error = errors.errorForField('classRef');
+            test.ok(errors.errorCount() > 0, "should have at least one validation message");
+            test.ok(error, "should have an error for the classRef field");
+            test.done();
+        }
+    },
+
+    match: {
+        matches_tag: function (test) {
+            var def = new MDPlus.Definition({
+                tag: 'H1',
+                classRef: true
+            });
+
+            jsdom.env('<h1>here</h1><h2>there</h2>', [], function (errors, window) {
+                var root = window.document.children[0];
+                var h1 = root.getElementsByTagName('h1')[0];
+                var h2 = root.getElementsByTagName('h2')[0];
+                test.ok(def.match(h1), "should match correct tag");
+                test.ok(!def.match(h2), "should not match incorrect tag");
+                test.done();
+            });
+        },
+
+        matches_lowercase_tag_definition: function (test) {
+            var def = new MDPlus.Definition({
+                tag: 'h1',
+                classRef: true
+            });
+
+            jsdom.env('<h1>here</h1>', [], function (errors, window) {
+                var root = window.document.children[0];
+                var h1 = root.getElementsByTagName('h1')[0];
+                test.ok(def.match(h1), "should match correct tag");
+                test.done();
+            });
         }
     }
 };
