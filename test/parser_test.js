a88e9217042f7d1568b8108afe89fe950d807f3f
Andrew Paradise <andrew@cantinaconsulting.com>
2012-08-02 10:27:51 -0400

    add some niceties based on attempts to use the parser

--- test/parser_test.js
+++ test/parser_test.js
@@ -51,10 +51,8 @@ exports.MDPlus = {
                 var html = [
                     '<div>', 
                     '<h1>Project Title</h1>', 
-                    '<h2>Stories</h2>', 
-                    '<p>Description of stories</p>', 
-                    '<h2>Deliverables</h2>', 
-                    '<p>Description of deliverables</p>', 
+                    '<h2>Stories</h2><p>Description of stories</p>', 
+                    '<h2>Deliverables</h2><p>Description of deliverables</p>', 
                     '</div>'].join("");
                 var testCase = function (errors, window) {
                     var root = window.document.children[0];
@@ -87,10 +85,8 @@ exports.MDPlus = {
                 var html = [
                     '<div>', 
                     '<h1>Project Title</h1>', 
-                    '<h2>Stories</h2>', 
-                    '<p>Description of stories</p>', 
-                    '<h2>Deliverables</h2>', 
-                    '<p>Description of deliverables</p>', 
+                    '<h2>Stories</h2><p>Description of stories</p>', 
+                    '<h2>Deliverables</h2><p>Description of deliverables</p>', 
                     '</div>'].join("");
                 var testCase = function (errors, window) {
                     var root = window.document.children[0];
@@ -118,6 +114,53 @@ exports.MDPlus = {
                     test.equal(callCount, 1, "should call the h1 handler once");
                     test.deepEqual(foundSpanLow, [0], "sets the low bound correctly")
                     test.deepEqual(foundSpanHigh, [1], "sets the high bound correctly")
+                    
+                    test.done();
+                };
+                jsdom.env(html, [], testCase);
+            },
+
+            should_parse_more_complex_trees: function (test) {
+                var html = [
+                    '<div>', 
+                    '<h1>Project Title</h1>', 
+                    '<h2>Stories</h2><p>Description of stories</p>', 
+                    '<h3>Deliverables</h3><p>Description of deliverables</p>', 
+                    '<h2>Other</h2><p>Description of other</p>', 
+                    '</div>'].join("");
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var div = root.getElementsByTagName('div')[0];
+                    var callCount = 0;
+                    var foundSpanLow, foundSpanHigh
+                    var handler = function (span) {
+                        callCount += 1;
+                        foundSpanLow = span.getLow();
+                        foundSpanHigh = span.getHigh();
+                    };
+                    var set = new MDPlus.Definition.Set([
+                        new MDPlus.Definition({
+                            tag: 'h1', handler: function () {},
+                            children: [
+                                new MDPlus.Definition({
+                                    tag: 'h2', handler: handler,
+                                    children: [
+                                        new MDPlus.Definition({
+                                            tag: 'h3', handler: function () {}
+                                        })
+                                    ]
+                                })
+                            ]
+                        })
+                    ]);
+                    set.bakeIDs();
+
+                    var parser = new MDPlus.Parser(div, set)
+                    
+                    parser.parse();
+                    test.equal(callCount, 2, "should call the h2 handler twice");
+                    test.deepEqual(foundSpanLow, [5], "sets the low bound correctly")
+                    test.deepEqual(foundSpanHigh, [], "sets the high bound correctly")
                     
                     test.done();
                 };
