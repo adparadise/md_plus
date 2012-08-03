6e94f4f99ccbe0dc828ad4c6ade29e440aad6a90
Andrew Paradise <andrew@cantinaconsulting.com>
2012-08-03 10:36:55 -0400

    allow regular expressions as definition matching tags.

--- test/definition_test.js
+++ test/definition_test.js
@@ -84,6 +84,24 @@ exports.MDPlus = {
                 jsdom.env(html, [], testCase);
             },
 
+            matches_regular_expressions: function (test) {
+                var def = new MDPlus.Definition({
+                    tag: /^h[0-9]$/i,
+                    handler: true
+                });
+                def.bakeIDs([]);
+                var html = '<h1>here</h1><h2>there</h2>'; 
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var h1 = root.getElementsByTagName('h1')[0];
+                    var h2 = root.getElementsByTagName('h2')[0];
+                    test.ok(def.match(h1), "should match correct tag");
+                    test.ok(def.match(h2), "should not match incorrect tag");
+                    test.done();
+                }
+                jsdom.env(html, [], testCase);
+            },
+
             matches_lowercase_tag_definition: function (test) {
                 var def = new MDPlus.Definition({
                     tag: 'h1',
