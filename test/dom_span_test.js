af784dc32ca69360bf5cc029ae0d6b54cf23edab
Andrew Paradise <andrew@cantinaconsulting.com>
2012-08-14 11:56:28 -0400

    add method to interpret json-encoded data within spans

--- test/dom_span_test.js
+++ test/dom_span_test.js
@@ -54,6 +54,105 @@ exports.MDPlus = {
                 };
                 jsdom.env(html, [], testCase);
             }
+        },
+
+        dataWithin: {
+            should_return_an_object: function (test) {
+                var location = [0];
+                var html = '<div><p>no data</p></div>';
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var div = root.getElementsByTagName('div')[0];
+                    var domTreeIterator = new MDPlus.DomTreeIterator(div);
+                    var domSpan = new MDPlus.DomSpan(domTreeIterator);
+                    domSpan.pushLocation([0]);
+                    domSpan.pushLocation([1]);
+
+                    var result = domSpan.dataWithin();
+
+                    test.equal(typeof(result), "object", "returns an object");
+                    test.done();
+                };
+                jsdom.env(html, [], testCase);
+            },
+            
+            should_include_json: function (test) {
+                var location = [0];
+                var html = '<div><p>some<code>{"value": "was"}</code></p></div>';
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var div = root.getElementsByTagName('div')[0];
+                    var domTreeIterator = new MDPlus.DomTreeIterator(div);
+                    var domSpan = new MDPlus.DomSpan(domTreeIterator);
+                    domSpan.pushLocation([0]);
+                    domSpan.pushLocation([1]);
+
+                    var result = domSpan.dataWithin();
+
+                    test.equal(result.value, "was", "includes correct data");
+                    test.done();
+                };
+                jsdom.env(html, [], testCase);
+            },
+
+            should_not_include_non_json: function (test) {
+                var location = [0];
+                var html = '<div><p>some<code>{"value": "was"}</code>non<code>not: {"other": "is"}</code></p></div>';
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var div = root.getElementsByTagName('div')[0];
+                    var domTreeIterator = new MDPlus.DomTreeIterator(div);
+                    var domSpan = new MDPlus.DomSpan(domTreeIterator);
+                    domSpan.pushLocation([0]);
+                    domSpan.pushLocation([1]);
+
+                    var result = domSpan.dataWithin();
+
+                    test.equal(result.value, "was", "includes correct data");
+                    test.equal(result.other, undefined, "does not include malformed data");
+                    test.done();
+                };
+                jsdom.env(html, [], testCase);
+            },
+
+            should_include_all_matching_tags: function (test) {
+                var location = [0];
+                var html = '<div><p>some<code>{"value": "was"}</code>non<code>{"other": "is"}</code></p></div>';
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var div = root.getElementsByTagName('div')[0];
+                    var domTreeIterator = new MDPlus.DomTreeIterator(div);
+                    var domSpan = new MDPlus.DomSpan(domTreeIterator);
+                    domSpan.pushLocation([0]);
+                    domSpan.pushLocation([1]);
+
+                    var result = domSpan.dataWithin();
+
+                    test.equal(result.value, "was", "includes correct data");
+                    test.equal(result.other, "is", "includes secondary data");
+                    test.done();
+                };
+                jsdom.env(html, [], testCase);
+            },
+
+            should_interpret_data_in_order: function (test) {
+                var location = [0];
+                var html = '<div><p>some<code>{"value": "was"}</code>non<code>{"value": "is"}</code></p></div>';
+                var testCase = function (errors, window) {
+                    var root = window.document.children[0];
+                    var div = root.getElementsByTagName('div')[0];
+                    var domTreeIterator = new MDPlus.DomTreeIterator(div);
+                    var domSpan = new MDPlus.DomSpan(domTreeIterator);
+                    domSpan.pushLocation([0]);
+                    domSpan.pushLocation([1]);
+
+                    var result = domSpan.dataWithin();
+
+                    test.equal(result.value, "is", "includes correct data");
+                    test.done();
+                };
+                jsdom.env(html, [], testCase);
+            }
         }
     }
 }
