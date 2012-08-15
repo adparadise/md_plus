749d77202c17a4f0e81201fe01c2122e4769062c
Andrew Paradise <andrew@cantinaconsulting.com>
2012-08-15 17:07:02 -0400

    updated examples reflecting use of builders

--- examples/stories.js
+++ examples/stories.js
@@ -8,57 +8,41 @@
 window.StoriesGenerator = function () {
     var MDPlus = require('md-plus');
 
-    var Story = function (name, tag) {
+    var Story = function (name, tag, data) {
         this._name = name;
         this._tag = tag;
+        this._data = data;
     };
 
-    // Alternate sequence definition for StackBuilder:
-    // Return depth by returning the header type minus one.
-    var sequence = function (span, definition) {
-        var tagName = span.getMatchingElement().tagName;
-        var pattern = /^h([0-9])$/i;
-        var match = pattern.exec(tagName);
-        if (match) {
-            return parseInt(match[1]) - 1;
-        }
-        return 0;
-    };
-    
     this.initialize = function (container) {
         this._container = container;
         this._stack = new MDPlus.StackBuilder();
         this._stack.setSequence(MDPlus.StackBuilder.HEADERS);
 
-        this._definition = new MDPlus.Definition.Set([
-            new MDPlus.Definition({
-                tag: /^h[0-9]$/i,
-                context: this._stack,
-                handler: this._stack.handler,
-                children: [
-                    new MDPlus.Definition({
-                        tag: 'li',
-                        context: this,
-                        handler: this.addStory
-                    })
-                ]
-            })
-        ]);
-        this._definition.bakeIDs();
+        var builder = new MDPlus.Definition.Builder();
+        this._definition = builder.build([{
+            tag: /^h[0-9]$/i,
+            context: this._stack,
+            handler: this._stack.handler,
+            children: [{
+                tag: 'li',
+                context: this,
+                handler: this.addStory
+            }]
+        }]);
     };
 
     this.addStory = function (span, definition) {
         var storyName = span.getMatchingElement().textContent;
         var tag = this._stack.getStack().join("_");
-        this._stories.push(new Story(storyName, tag));
+        var data = span.dataWithin();
+        this._stories.push(new Story(storyName, tag, data));
     };
 
     this.consume = function () {
         var parser = new MDPlus.Parser(this._container, this._definition);
         this._stories = [];
         parser.parse();
-        
-        console.log(this._stories);
     }    
 
     this.initialize.apply(this, arguments);
